dd3954d2f39bfa1274d00268d09e4e82a5873764
calmanzab <64765773+carlosalmanzab@users.noreply.github.com>
2024-01-27 17:00:30 -0500

    add new container breadcrumb component

--- src/app/modules/panel/home/home.component.ts
+++ src/app/modules/panel/home/home.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from "./shared/header/header.component";
 import { SideBarComponent } from "./shared/side-bar/side-bar.component";
 import { RouterOutlet } from '@angular/router';
+import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [HeaderComponent, SideBarComponent, RouterOutlet],
+    imports: [HeaderComponent, SideBarComponent, RouterOutlet, BreadcrumbComponent],
     styleUrl: './home.component.css',
     templateUrl: './home.component.html'
 })
