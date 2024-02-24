eabecdd9e81b3301988c1e016c07f86761317135
calmanzab <64765773+carlosalmanzab@users.noreply.github.com>
2024-02-23 23:44:29 -0500

    feat(table): :lipstick: new edit mode

--- src/app/modules/panel/home/home.component.ts
+++ src/app/modules/panel/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './shared/header/header.component';
 import { SideBarComponent } from './shared/side-bar/side-bar.component';
 import { RouterOutlet } from '@angular/router';
 import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
 import { MenuActionTableComponent } from '../views/shared/menu-action-table/menu-action-table.component';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
 	selector: 'app-home',
@@ -12,4 +13,30 @@ import { MenuActionTableComponent } from '../views/shared/menu-action-table/menu
 	styleUrl: './home.component.css',
 	templateUrl: './home.component.html'
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+	constructor(private primeNGConfig: PrimeNGConfig) {}
+
+	ngOnInit(): void {
+		this.primeNGConfig.setTranslation({
+			matchAll: 'Coincidir Con Todos',
+			matchAny: 'Coincidir Con Alguno',
+			startsWith: 'Comienza Con',
+			contains: 'Contiene',
+			notContains: 'No Contiene',
+			endsWith: 'Termina Con',
+			equals: 'Igual A ',
+			notEquals: 'Diferente A ',
+			dateIs: 'La Fecha Es',
+			dateIsNot: 'La Fecha No Es',
+			dateBefore: 'Fecha Es Antes De',
+			dateAfter: 'Fecha Es Posterior De ',
+			lt: 'Menor Que',
+			lte: 'Menor o Igual Que',
+			gt: 'Mayor Que',
+			gte: 'Mayor o Igual Que',
+			addRule: 'Agregar Filtro',
+			clear: 'Limpiar',
+			apply: 'Aplicar'
+		});
+	}
+}
--- src/app/modules/panel/views/shared/stock-table/stock-table.component.ts
+++ src/app/modules/panel/views/shared/stock-table/stock-table.component.ts
@@ -1,69 +1,66 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ProductoEntrada } from '@domain/productoEntrada.model';
 import { ProductoSalida } from '@domain/productoSalida.model';
 import { ProductoStock } from '@domain/productoStock.model';
-import { InputSwitchModule } from 'primeng/inputswitch';
-import { Table, TableModule } from 'primeng/table';
-import { TagModule } from 'primeng/tag';
-import { StockService } from './../../control-inventario/services/stock.service';
-import { PrimeNGConfig } from 'primeng/api';
+import { StockService } from '@views/control-inventario/services/stock.service';
 import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputNumber, InputNumberModule } from 'primeng/inputnumber';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { Table, TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
 
 @Component({
 	selector: 'app-stock-table',
 	standalone: true,
 	imports: [
-		TableModule,
-		InputSwitchModule,
 		TagModule,
+		TableModule,
+		FormsModule,
 		CommonModule,
 		ButtonModule,
 		InputTextModule,
 		InputGroupModule,
+		InputNumberModule,
+		InputSwitchModule,
 		InputGroupAddonModule
 	],
 	templateUrl: './stock-table.component.html',
 	styleUrl: './stock-table.component.css'
 })
 export class StockTableComponent implements OnInit {
+	@ViewChildren(InputNumber) inputNumbers?: QueryList<InputNumber>;
+	// contenido de la tabla
 	productos: ProductoEntrada[] | ProductoSalida[] | ProductoStock[] = [];
-
-	typeProductos?: unknown;
-
+	// productos seleccionados
 	selectedProductos!: ProductoEntrada[] | ProductoSalida[] | ProductoStock[];
+	//clon de producto para edicion
+	clonedProductos: { [s: string]: ProductoEntrada | ProductoSalida | ProductoStock } = {};
 
-	constructor(
-		private readonly stockService: StockService,
-		private primeNGConfig: PrimeNGConfig
-	) {}
+	constructor(private readonly stockService: StockService) {}
 
 	ngOnInit(): void {
 		this.productos = this.stockService.get();
-		this.primeNGConfig.setTranslation({
-			matchAll: 'Coincidir Con Todos',
-			matchAny: 'Coincidir Con Alguno',
-			startsWith: 'Comienza Con',
-			contains: 'Contiene',
-			notContains: 'No Contiene',
-			endsWith: 'Termina Con',
-			equals: 'Igual A ',
-			notEquals: 'Diferente A ',
-			dateIs: 'La Fecha Es',
-			dateIsNot: 'La Fecha No Es',
-			dateBefore: 'Fecha Es Antes De',
-			dateAfter: 'Fecha Es Posterior De ',
-			lt: 'Menor Que',
-			lte: 'Menor o Igual Que',
-			gt: 'Mayor Que',
-			gte: 'Mayor o Igual Que',
-			addRule: 'Agregar Filtro',
-			clear: 'Limpiar',
-			apply: 'Aplicar'
+	}
+
+	setInputNumberWith() {
+		const inputElements = this.getInputNumberStyle();
+		inputElements.forEach((iE) => {
+			(iE as ElementRef).nativeElement().style.width = '100px';
 		});
+	}
+
+	getInputNumberStyle() {
+		const inputElements: unknown[] = [];
+		this.inputNumbers?.forEach((inputN) => {
+			const inputElement = (inputN.el as ElementRef).nativeElement.querySelector('p-inputnumber > span > input');
+			inputElements.push(inputElement);
+		});
+		return inputElements;
 	}
 	isProductoEntrada() {
 		if (this.productos.length > 0) {
@@ -116,5 +113,29 @@ export class StockTableComponent implements OnInit {
 		}
 
 		return field;
+	}
+	onRowEditInit(producto: ProductoEntrada | ProductoSalida | ProductoStock) {
+		this.clonedProductos[producto.codigo as unknown as string] = { ...producto };
+		this.setInputNumberWith();
+	}
+
+	onRowEditSave(producto: ProductoEntrada | ProductoSalida | ProductoStock) {
+		if (
+			this.validStock((producto as ProductoEntrada).cantidadEntrada) ||
+			this.validStock((producto as ProductoSalida).cantidadSalida) ||
+			(this.validStock((producto as ProductoStock).cantidadEntrada) &&
+				this.validStock((producto as ProductoStock).cantidadSalida))
+		) {
+			delete this.clonedProductos[producto.codigo as unknown as string];
+		}
+	}
+
+	private validStock(stock?: number): boolean {
+		return stock !== undefined && stock >= 0;
+	}
+
+	onRowEditCancel(producto: ProductoEntrada | ProductoSalida | ProductoStock, index: number) {
+		this.productos[index] = this.clonedProductos[producto.codigo as unknown as string];
+		delete this.clonedProductos[producto.codigo as unknown as string];
 	}
 }
